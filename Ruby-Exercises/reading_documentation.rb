b53cb9abfb2fcefff5b406e00d159480b020e525
Steve De Jongh <de.jongh.steve@gmail.com>
2022-11-22 17:08:30 -0800

    Reading Doc section

---
+++ Ruby-Exercises/reading_documentation.rb
@@ -0,0 +1,11 @@
+=begin
+1.  www.ruby-doc.org or https://docs.ruby-lang.org/en/
+2. Control expressions https://docs.ruby-lang.org/en/2.7.0/syntax/control_expressions_rdoc.html
+3. Loops return nil unless break is used to supply value.
+4. break returns nil when no arguments are passed to break, or the value of the argument when an argument is provided.
+5. underscores can be used to break up numbers, just like commas in written form.
+7. Use the collon to indicate a symbol ":steve"
+8. https://ruby-doc.org/core-3.1.2/String.html or https://docs.ruby-lang.org/en/2.7.0/String.html
+9. #rjust
+
+=end
